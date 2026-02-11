c07c52ccc129f24704faa7ff1a7d011aaca696fb
Tom <red.shirt083@passinbox.com>
2026-02-11 10:10:14 -0800

    added string service tests.

---
+++ src/Project498.WebApi.Tests/StringServiceTests.cs
@@ -0,0 +1,25 @@
+using Project498.WebApi.Services;
+
+namespace Project498.WebApi.Tests;
+
+public class StringServiceTests
+{
+    private readonly StringService _stringService = new();
+
+    [Theory]
+    [InlineData("hello", "olleh")]
+    [InlineData("", "")]
+    [InlineData(null, "")]
+    [InlineData("a", "a")]
+    [InlineData("racecar", "racecar")]
+    [InlineData("hello world", "dlrow olleh")]
+    [InlineData("hello  world", "dlrow  olleh")]
+    [InlineData(" hello world", "dlrow olleh ")]
+    [InlineData("hello world ", " dlrow olleh")]
+    public void Reverse_WithVariousInputs_ReturnsExpectedResult(string? input, string expected)
+    {
+        var result = _stringService.Reverse(input!);
+
+        Assert.Equal(expected, result);
+    }
+}
